d6d13d33ff0f2f1d111f515ab2e6475e814c6868
Jeet <jitender2650817@gmail.com>
2018-03-14 21:55:03 +0530

    managing sessions

---
+++ Managing Sessions/server.py
@@ -0,0 +1,34 @@
+from flask import Flask, session, redirect, url_for, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'Your Secret Key Here'
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 1234)
